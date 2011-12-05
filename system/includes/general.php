9b4eb8d3777d862367fadc14f3090c934004ffa0
Olof Fredriksson <oloflolof@gmail.com>
2011-12-05 15:23:34 +0100

    Added function the_siteinfo()

--- system/includes/general.php
+++ system/includes/general.php
@@ -12,11 +12,19 @@ function get_siteInfo($value = "") {
 			$output = $config["uri_prefix"];
 		break;
 		
+		case "stylesheet_url":
+			$output = $config["url"]."/site/themes/".$config["theme"]."/style.css";
+		break;
+		
 		case "default_controller":
 			$output = $config["default_controller"];
 		break;
 	}
 	
 	return $output;
+}
+
+function the_siteInfo($value) {
+	echo get_siteInfo($value);
 }
 ?>
