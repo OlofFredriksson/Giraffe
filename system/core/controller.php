9eb053f0f3402814e07dd86f78a638c1a42513d3
Olof Fredriksson <oloflolof@gmail.com>
2011-12-05 19:32:45 +0100

    Controllers functions is now protected

--- system/core/controller.php
+++ system/core/controller.php
@@ -2,14 +2,14 @@
 class Controller {
 	function __construct() {
 	}
-	function loadView($view,$data="") {
+	 protected function loadView($view,$data="") {
 		if(is_array($data) && count($data) > 0) {
 			extract($data);
 		}
 		require_once('site/themes/'.get_siteInfo("theme").'/'.$view.'.php');
 	}
 	
-	function loadModel($model) {
+	protected function loadModel($model) {
 		require_once('model/'.$model.'.php');
 		$this->$model = new $model;
 	}
