f04810f4dbe06ba6e6e9e8ca90c618b0015030f2
Olof Fredriksson <fredriksson@olof.nu>
2012-01-03 21:03:53 +0100

    Added comment in 404 controller

--- system/controllers/error_404.php
+++ system/controllers/error_404.php
@@ -1,13 +1,14 @@
 <?php
 class error_404 extends Controller {
 	function index() {
-		$data["site_title"] = "404";
+		// First, check so the current theme contains a 404 page, if not just print out simple 404 text.
 		$file_name = SITE_PATH."/themes/".get_siteInfo("theme")."/404.php";
 		if (file_exists($file_name)) {
 			require_once($file_name);
 		} else {
 			echo "404 - File not found";
 		}
+		$data["site_title"] = "404";
 		$this->loadView('404',$data);
 	}
 }
