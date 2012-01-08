73eef40f2cb8ffa16dc40a44965f7fca47cec452
Olof Fredriksson <fredriksson@olof.nu>
2012-01-08 14:52:57 +0100

    Updated readme

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ MVC CMS for dv1207 at Blekinge Institute of Technology
 -   Apache (Optional: Clean urls with mod_rewrite)
 -   Php 5 
 -   Mysql (mysqli)
+
+## License ##
+
+### Bootstrap
+Copyright 2011 Twitter, Inc.
+Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
--- system/core/auth.php
+++ system/core/auth.php
@@ -3,7 +3,11 @@ class Auth {
 	// Makes it possible to use a secondary or external database
 	private $db;
 	private function __construct($db = null) {
-	
+		global $giraffe;
+		// If $db is null, we take same database as Giraffe use
+		if(empty($db)) {
+			$this->db = $giraffe->db
+		}
 	}
 }
 ?>
