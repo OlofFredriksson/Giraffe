2cb71c813d3bb933c03d5f7ad30804a70092b805
Olof Fredriksson <fredriksson@olof.nu>
2012-01-09 15:11:18 +0100

    Fixed typo's and added update post function

--- site/models/cms.php
+++ site/models/cms.php
@@ -24,20 +24,28 @@ class Cms {
 	public function get_post_with_id($id) {
 		
 		$post = array();
-		$slug = $this->db->escape($slug);
+		$id = $this->db->escape($id);
 		$query = "SELECT * FROM ".DB_PREFIX."post WHERE id = '".$id."'";
 		$result = $this->db->get_results($query);
 		if(!$result->num_rows == 1) {
-			throw new Exception('Post with slug '.$slug.' is not found');
+			throw new Exception('Post with id '.$id.' is not found');
 		}
 		$row = $result->fetch_object();
+		$post["slug"] = $row->slug;
 		$post["title"] = $row->title;
 		$post["content"] = $row->content;
 		$post["id"] = $row->id;
 		$post["date"] = $row->date;
 		return $post;
 	}
-	
+	public function update_post($id, $title,$slug,$content) {
+		$id = $this->db->escape($id);
+		$title = $this->db->escape($title);
+		$slug = $this->db->escape($slug);
+		$content = $this->db->escape($content);
+		$query = "UPDATE ".DB_PREFIX."post SET title = '".$title."', slug = '".$slug."', content = '".$content."' WHERE id = '".$id."' LIMIT 1";
+		return $this->db->query($query);
+	}
 	public function get_post_list() {
 		$query = "SELECT * FROM ".DB_PREFIX."post";
 		return $this->db->query($query);
