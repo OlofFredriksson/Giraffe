41fa3dddd01766fe466799ed8f420995b36c0073
Olof Fredriksson <fredriksson@olof.nu>
2012-01-10 16:43:03 +0100

    Added region function

--- system/includes/theme.php
+++ system/includes/theme.php
@@ -19,3 +19,26 @@ function get_file($file) {
 	global $giraffe;
 	$giraffe->getController()->load->view($region);
 }
+
+function get_regions($name = "") {
+	global $giraffe;
+	if(empty($name)) {
+		$regions = array();
+		$query = "SELECT * FROM ".DB_PREFIX."region";
+		$result = $giraffe->db->query($query);
+		while($row = $result->fetch_object()) {
+			$regions[$row->name] = $row->content;
+		}
+	} else {
+		$regions = array();
+		$query = "SELECT * FROM ".DB_PREFIX."region WHERE name = '".$name."'";
+		$result = $giraffe->db->query($query);
+		if($result->num_rows == 1) {
+			$row = $result->fetch_object();
+			$regions = $row->content;
+		}
+	}
+	
+	return $regions;
+}
+?>
